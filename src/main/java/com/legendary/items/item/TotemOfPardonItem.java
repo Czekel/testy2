347cd273e79bbabb5228d192d7a715a4b80b654c
Czekel <ignacypalinski995@gmail.com>
2026-08-19 15:54:35 +0200

    Add files via upload

---
+++ src/main/java/com/legendary/items/item/TotemOfPardonItem.java
@@ -0,0 +1,9 @@
+package com.legendary.items.item;
+
+import net.minecraft.item.Item;
+
+public class TotemOfPardonItem extends Item {
+    public TotemOfPardonItem(Settings settings) {
+        super(settings);
+    }
+}
